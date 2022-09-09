30a6e6b27ab82154ec0df75597a4ee175b6c24bc
sxxgxxnz <tmddms2292@gmail.com>
2022-09-09 17:37:57 +0900

    비밀지도 코드수정(스택으로 변경)

--- programmers/kakao_2018/1-13.cpp
+++ programmers/kakao_2018/1-13.cpp
@@ -1,28 +1,34 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
 vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
     vector<string> answer;
+    stack<char> str;
 
     for(int i=0;i<n;i++){
-        string tmp = "";
+        //string tmp = "";
         arr1[i] = arr1[i] | arr2[i];
 
-        while(tmp.size()!=n){
+        for(int j=0;j<n;j++){
             if(arr1[i] % 2 == 0){
-                tmp.push_back(' ');
+                str.push(' ');
 
             }
             else{
-                tmp.push_back('#');
+                str.push('#');
             }
             arr1[i] /= 2;
         }
-        reverse(tmp.begin(),tmp.end());
-        answer.push_back(tmp);
+
+        answer.push_back("");
+        while(!str.empty()){
+            answer[i].push_back(str.top());
+            str.pop();
+        }
     }
     return answer;
 }
